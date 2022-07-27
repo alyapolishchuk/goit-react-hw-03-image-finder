3f2ee1cc5cb903b7b6cd37303ce4b3351a8d7927
alyapolishchuk <alinochkarul081@gmail.com>
2022-07-27 20:52:51 +0300

    c

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ImgGal } from './ImageGallery/ImageGallery';
+import { Modal } from './Modal/Modal';
+import { Searchbar } from './Searchbar/Searchbar';
+
+export class App extends Component {
+  state = {
+    image: '',
+    query: '',
+  };
+  openModal = img => {
+    this.setState({ image: img });
+  };
+  closeModal = () => {
+    this.setState({ image: '' });
+  };
+  handlerForm = query => {
+    this.setState({ query });
+  };
+
+  render() {
+    const { query, image } = this.state;
+    return (
+      <div
+        style={
+          {
+            // display: 'grid',
+            // gridTemplateColumns: '1fr0',
+            // gridGap: '16px',
+            // paddingBottom: '24px',
+          }
+        }
+      >
+        <Searchbar onSubmit={this.handlerForm} />
+
+        <ImgGal query={query} openModal={this.openModal} />
+
+        {image && <Modal image={image} onClose={this.closeModal} />}
+        <ToastContainer />
+      </div>
+    );
+  }
+}
