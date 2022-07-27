05f62917aa90f20852df6b2688c0da7e36a67aba
alyapolishchuk <alinochkarul081@gmail.com>
2022-07-27 20:47:23 +0300

    c

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+
+class Modal extends Component {
+  static propTypes = {
+    image: PropTypes.string.isRequired,
+    onClose: PropTypes.func.isRequired,
+  };
+  componentDidMount() {
+    window.addEventListener('keydown', this.onTap);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.onTap);
+  }
+  onTap = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+  openBackdrop = e => {
+    if (e.target === e.currentTarget) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return (
+      <div onClick={this.openBackdrop}>
+        <div>
+          <img src={this.props.image} alt="" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export { Modal };
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,19 +1,62 @@
 import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { toast } from 'react-toastify';
 
-export default function Searchbar({ onSubmit }) {}
+class Searchbar extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
+  state = {
+    query: '',
+  };
 
-<header class="searchbar">
-  <form class="form">
-    <button type="submit" class="button">
-      <span class="button-label">Search</span>
-    </button>
+  handlerInput = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
-    <input
-      class="input"
-      type="text"
-      autocomplete="off"
-      autofocus
-      placeholder="Search images and photos"
-    />
-  </form>
-</header>;
+  handlerSubmit = e => {
+    const { query } = this.state;
+    e.preventDefault();
+
+    if (!query.trim()) {
+      toast.error('empty field', {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      return;
+    }
+    this.props.onSubmit(query);
+    this.setState({ query: '' });
+  };
+
+  render() {
+    const { query } = this.state;
+    return (
+      <header class="searchbar">
+        <form class="form">
+          <button type="submit" class="button">
+            <span class="button-label">Search</span>
+          </button>
+
+          <input
+            class="input"
+            type="text"
+            name={query}
+            value={query}
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export { Searchbar };
