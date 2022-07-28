b3ba88123465d9984a223c64e31039f0b7871a4b
alyapolishchuk <alinochkarul081@gmail.com>
2022-07-28 14:53:02 +0300

    c

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,16 @@
-import { FallingLines } from 'react-loader-spinner';
+import { MutatingDots } from 'react-loader-spinner';
 const Loader = () => {
   return (
-    <FallingLines
-      color="#4d55a9"
+    <MutatingDots
+      height="100"
       width="100"
+      color="#4fa94d"
+      secondaryColor="#4fa94d"
+      radius="12.5"
+      ariaLabel="mutating-dots-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
       visible={true}
-      ariaLabel="falling-lines-loading"
     />
   );
 };
